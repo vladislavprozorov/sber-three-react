21287bf6b87a87d8ed5b19f07b9f47db3502f49d
vladislavprozorov <slawasam@mail.ru>
2026-03-02 13:30:53 -0800

    feat(ThreeScene): add cube with animation

--- src/components/ThreeScene.tsx
+++ src/components/ThreeScene.tsx
@@ -10,7 +10,21 @@ export default function ThreeScene(){
         renderer.setSize(containerRef.current.clientWidth, containerRef.current.clientHeight);
         containerRef.current.appendChild(renderer.domElement);
         camera.position.z = 5;
+        const box = new THREE.BoxGeometry(2, 2, 2);
+        const material = new THREE.MeshStandardMaterial({color: "green"});
+        const mesh = new THREE.Mesh(box, material);
+        scene.add(mesh);
+        const light = new THREE.AmbientLight("white", 1);
+        light.position.set(5, 5, 5);
+        scene.add(light);
         renderer.render(scene, camera);
+        function animate(){
+            requestAnimationFrame(animate);
+            mesh.rotation.x += 0.01;
+            mesh.rotation.y += 0.01;
+            renderer.render(scene, camera);
+        }
+        animate();
     }, [])    
     return (
         <div style={{width: "100%", height: "100vh"}} ref={containerRef}></div>
